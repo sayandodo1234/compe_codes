a4097c9222f6fe9251a6e64c8df53590e869e9ba
sayandodo1234 <51147454+sayandodo1234@users.noreply.github.com>
2020-09-21 12:22:29 +0530

    Add files via upload

---
+++ unique_characters.cpp
@@ -0,0 +1,44 @@
+// Given a string, you need to remove all the duplicates. That means, the output string should contain each character only once. The respective order of characters should remain same.
+// Input format :
+// String S
+// Output format :
+// Output String
+// Constraints :
+// 0 <= Length of S <= 10^8
+// Sample Input 1 :
+// ababacd
+// Sample Output 1 :
+// abcd
+// Sample Input 2 :
+// abcde
+// Sample Output 2 :
+// abcde
+
+#include<bits/stdc++.h>
+using namespace std;
+char* uniqueChar(char *str){
+    // Write your code here
+    int n= strlen(str);
+    if(n==0)
+    {
+        char *temp = new char[1];
+        temp[0] = '\0';
+        return temp;
+    }
+    char *temp = new char[n+1];
+    unordered_map<char,bool> m;
+    int k=0;
+    for(int i=0;i<n;i++)
+    {
+        char buff = str[i];
+        if(m.count(buff) == 0)
+        {
+            m[buff] = true;
+            temp[k] = buff;
+            k++;
+        }
+    }
+    temp[k] = '\0';
+    return temp;
+
+}
