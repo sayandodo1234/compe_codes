c8743bb06501e75a6cb0b15cc7e7fdd9d839a765
sayandodo1234 <51147454+sayandodo1234@users.noreply.github.com>
2021-01-31 20:19:09 +0530

    Add files via upload

--- Graphs/AdvancedGraphs/fillmtr.cpp
+++ Graphs/AdvancedGraphs/fillmtr.cpp
@@ -40,66 +40,156 @@
 
 
 #include<bits/stdc++.h>
-#define MAXN 100000
 using namespace std;
-int setfa[MAXN*2+5];
-void Init()
+
+class edges{
+    public:
+    int s;
+    int e;
+    edges(int start,int end){
+        s=start;
+        e=end;
+    }
+};
+class Disj{
+    public:
+  	int parent;
+  	int rank;
+};
+
+int find(int child,Disj *dj)
 {
-	for(int i=0;i<=MAXN*2+3;i++)
-		setfa[i]=i;
+  	if(dj[child].parent != child)
+    {
+      	dj[child].parent = find(dj[child].parent,dj);
+    }
+  	return dj[child].parent;
 }
-int Findfa(int x)
+
+void Union(int x,int y,Disj *ds)
 {
-	if(x==setfa[x])
-		return x;
-	return setfa[x]=Findfa(setfa[x]);
+  	int rootX = find(x,ds);
+    int rootY = find(y,ds);
+
+  	if(rootX == rootY)
+      	return;
+  	
+    if(ds[rootX].rank < ds[rootY].rank)
+    {
+        ds[rootX].parent = rootY;
+    }
+    else if(ds[rootX].rank > ds[rootY].rank)
+    {
+        ds[rootY].parent = rootX;
+    }
+    else
+    {
+        ds[rootX].parent = rootY;
+        ds[rootY].rank++;
+    }
 }
-void Union(int x,int y)
+
+bool isUnion(int x,int y, Disj *dj)
 {
-	int fx=Findfa(x),fy=Findfa(y);
-	if(fx==fy)
-		return;
-	setfa[fx]=fy;
+  	return find(x,dj) == find(y,dj);
 }
-bool Same(int x,int y)
-{
-	return Findfa(x)==Findfa(y);
-}
-int main()
-{
-	int times;
-	int N,Q;
-	scanf("%d",&times);
-	for(int tmn=1;tmn<=times;tmn++)
-	{
-		Init();
-		scanf("%d %d",&N,&Q);
-		int x,y,z; 
-		for(int i=1;i<=Q;i++)
-		{
-			scanf("%d %d %d",&x,&y,&z);
-			if(z==0)
-			{
-				Union(x,y);
-				Union(x+MAXN,y+MAXN);
-			}
-			else if(z==1)
-			{
-				Union(x,y+MAXN);
-				Union(y,x+MAXN);
+bool bipartite(vector<int>* edges, int n) {
+	if (n == 0) {
+		return true;
+	}
+	unordered_set<int> sets[2];
+	vector<int> pending;
+	sets[0].insert(0);
+	pending.push_back(0);
+	while (pending.size() > 0) {
+		int current = pending.back();
+		pending.pop_back();
+		int currentSet = sets[0].count(current) > 0 ? 0 : 1;
+		for (int i = 0; i < edges[current].size(); i++) {
+			int neighbor = edges[current][i];
+			if (sets[0].count(neighbor) == 0 && sets[1].count(neighbor) == 0) {
+				sets[1 - currentSet].insert(neighbor);
+				pending.push_back(neighbor);
+			} else if (sets[currentSet].count(neighbor) > 0) {
+				return false;
 			}
 		}
-		bool flag=true;
-		for(int i=1;i<=MAXN;i++)
-			if(Same(i,i+MAXN)==true)
-			{
-				flag=false;
-				break;
-			}
-		if(flag==true)
-			printf("yes\n");
-		else
-			printf("no\n");
 	}
+	return true;
+}
+
+
+int main()
+{
+	//code
+  	int t;
+  	cin >> t;
+  
+  	while(t--)
+    {
+      	int n,q;
+      	cin >> n >> q;
+      
+      	vector<int> *graph = new vector<int>[n];
+      	vector<pair<int,int>> oneDiff;
+      	
+     	Disj *dj = new Disj[n];
+      	for(int i = 0; i < n; i++)
+        {
+          	dj[i].parent = i;
+          	dj[i].rank = 0;
+        }
+        vector<edges> Edges;
+      
+        bool sanity_check = true;
+      
+      	for(int i = 0; i < q; i++)
+        {
+          	int a,b,c;
+          	cin >> a >> b >> c;
+          	a--;
+          	b--;
+          	
+          	if(a == b && c != 0)
+              	sanity_check = false;
+          	
+          	if(c == 0)
+            {
+              	Union(a,b,dj);
+            }
+          	else
+            {
+                Edges.push_back(edges(a,b)) ;
+            }
+        }
+      
+
+        for(int i = 0 ; i < Edges.size(); i++){
+            int e1 = find(Edges[i].s,dj) ;
+            int e2 = find(Edges[i].e,dj) ;
+            if(e1 == e2){
+                sanity_check = false;
+                break;
+            }
+            graph[e2].push_back(e1) ;
+        }
+
+      
+      	if(!sanity_check)
+        {
+          	cout << "no" << endl;
+          	continue;
+        }
+      
+      	bool ans = bipartite(graph,n);
+      	if(ans)
+        {
+          	cout << "yes" << endl;
+        }
+      	else
+        {
+          	cout << "no" << endl;
+        }
+    }
 	return 0;
 }
